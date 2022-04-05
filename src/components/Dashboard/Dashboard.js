56c31f937bb738d3242ab88f7b66765cd034ff5a
Toufiq-it <ittoufikit@gmail.com>
2022-04-05 20:22:41 +0600

    4th commit

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis } from 'recharts';
+import './Dashboard.css'
 
 const Dashboard = () => {
+    const [charts , setCharts] = useState([]);
+    useEffect(()=>{
+        fetch('rechart.json')
+        .then(res=>res.json())
+        .then(data=>setCharts(data));
+    },[]);
     return (
         <div>
-            <h2>This is Dashboard page</h2>
+            <h1>Investment VS Revenue</h1>
+            <div className='chart-alignment'>
+            <div>
+                <BarChart
+                    width={600}
+                    height={400}
+                    data={charts}
+                    margin={{
+                    top: 20,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis dataKey="investment" />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="investment" stackId="a" fill="#8884d8" />
+                    <Bar dataKey="revenue" stackId="a" fill="#82ca9d" />
+                </BarChart>
+            </div>
+            <div>
+            <AreaChart
+                width={600}
+                height={400}
+                data={charts}
+                margin={{
+                    top: 10,
+                    right: 30,
+                    left: 0,
+                    bottom: 0,
+                }}
+                >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="month" />
+                <YAxis dataKey="investment" />
+                <Tooltip />
+                <Area type="monotone" dataKey="investment" stackId="1" stroke="#8884d8" fill="#8884d8" />
+                <Area type="monotone" dataKey="revenue" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
+                </AreaChart>
+            </div>
+            </div>
         </div>
     );
 };
--- src/components/Notfound/NotFound.js
+++ src/components/Notfound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>Not Found ! 404 </h2>
+        <div className='not-found'>
+            <h1> 404 </h1>
+            <h3>Sorry ! This Page is Not Found</h3>
         </div>
     );
 };
